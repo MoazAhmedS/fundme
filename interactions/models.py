efa6b7dd262609ea396a897f762e175513d60302
Ebramrasmy <ebramrasmy7@gmail.com>
2025-05-25 12:42:02 +0300

    edit models

--- interactions/models.py
+++ interactions/models.py
@@ -14,28 +14,28 @@ class Tag(models.Model):
 
 
 class ProjectTag(models.Model):
-    project = models.ForeignKey(Project, on_delete=models.CASCADE, verbose_name="Project")
-    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, verbose_name="Tag")
+    project_id = models.ForeignKey(Project, on_delete=models.CASCADE, verbose_name="Project")
+    tag_id = models.ForeignKey(Tag, on_delete=models.CASCADE, verbose_name="Tag")
 
     class Meta:
         verbose_name = "Project Tag"
         verbose_name_plural = "Project Tags"
-        unique_together = ('project', 'tag') 
+        unique_together = ('project_id', 'tag_id') 
 
     def __str__(self):
         return f"{self.project.title} - {self.tag.name}"
 
 
 class Rate(models.Model):
-    user = models.ForeignKey(ProfileUser, on_delete=models.CASCADE, verbose_name="User")
-    project = models.ForeignKey(Project, on_delete=models.CASCADE, verbose_name="Project")
+    user_id = models.ForeignKey(ProfileUser, on_delete=models.CASCADE, verbose_name="User")
+    project_id = models.ForeignKey(Project, on_delete=models.CASCADE, verbose_name="Project")
     rate_value = models.PositiveSmallIntegerField(verbose_name="Rate Value")  
     note = models.TextField(blank=True, null=True, verbose_name="Note")
 
     class Meta:
         verbose_name = "Rate"
         verbose_name_plural = "Rates"
-        unique_together = ('user', 'project')  
+        unique_together = ('user_id', 'project_id')  
 
     def __str__(self):
         return f"{self.user.username} rated {self.project.title} - {self.rate_value}"
