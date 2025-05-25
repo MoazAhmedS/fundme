8455b83085e1191fe12a19712673aae8b674be52
mahmoudOraby <mahmoud.eelu.2020@gmail.com>
2025-05-25 09:56:35 +0300

    make migration and final model

--- accounts/models.py
+++ accounts/models.py
@@ -4,52 +4,19 @@ from django.core.validators import RegexValidator
 
 class ProfileUser(AbstractUser):
     phone_regex = RegexValidator(
-        regex=r'^01[0125][0-9]{8}$',
-        message="Phone number must be a valid Egyptian number starting with 010, 011, 012, or 015"
-    )
+        regex=r'^01[0125][0-9]{8}$', message="Phone number must be a valid Egyptian number starting with 010, 011, 012, or 015")
     
-    phone = models.CharField(
-        validators=[phone_regex], 
-        max_length=11, 
-        unique=True,
-        verbose_name="Phone Number"
+    phone = models.CharField( validators=[phone_regex],  max_length=11, unique=True, verbose_name="Phone Number"
     )
-    image = models.ImageField(
-        upload_to='profile_pics/', 
-        blank=True, 
-        null=True,
-        verbose_name="Profile Picture"
-    )
+    image = models.ImageField(upload_to='profile_pics/',  blank=True, null=True, verbose_name="Profile Picture")
     
-    birth_date = models.DateField(
-        blank=True, 
-        null=True,
-        verbose_name="Birth Date"
-    )
-    facebook = models.URLField(
-        max_length=255, 
-        blank=True, 
-        null=True,
-        verbose_name="Facebook Profile"
-    )
-    country = models.CharField(
-        max_length=100, 
-        blank=True, 
-        null=True,
-        verbose_name="Country"
-    )
+    birth_date = models.DateField(blank=True, null=True, verbose_name="Birth Date")
+    facebook = models.URLField( max_length=255,  blank=True, null=True,verbose_name="Facebook Profile")
+    country = models.CharField( max_length=100,   blank=True, null=True,verbose_name="Country")
     
-
+    email_active = models.BooleanField(default=False, verbose_name="Email Activated")
     
-    email_active = models.BooleanField(
-        default=False,
-        verbose_name="Email Activated"
-    )
-    
-    create_date = models.DateTimeField(
-        auto_now_add=True,
-        verbose_name="Creation Date"
-    )
+    create_date = models.DateTimeField( auto_now_add=True,verbose_name="Creation Date")
     
     def __str__(self):
         return self.username
