ca7f8a6d3c368385b8041bbb7547b1ac181a6a65
mahmoudOraby <mahmoud.eelu.2020@gmail.com>
2025-05-25 08:56:30 +0300

    ubdate on profileUser model

--- accounts/models.py
+++ accounts/models.py
@@ -2,27 +2,68 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.core.validators import RegexValidator
 
-
 class ProfileUser(AbstractUser):
     phone_regex = RegexValidator(
         regex=r'^01[0125][0-9]{8}$',
         message="Phone number must be a valid Egyptian number starting with 010, 011, 012, or 015"
     )
     
-    phone = models.CharField(validators=[phone_regex], max_length=11, unique=True)
-    image = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
-    birth_date = models.DateField(blank=True, null=True)
-    facebook = models.URLField(max_length=255, blank=True, null=True)
-    country = models.CharField(max_length=100, blank=True, null=True)
-
+    phone = models.CharField(
+        validators=[phone_regex], 
+        max_length=11, 
+        unique=True,
+        verbose_name="Phone Number"
+    )
+    image = models.ImageField(
+        upload_to='profile_pics/', 
+        blank=True, 
+        null=True,
+        verbose_name="Profile Picture"
+    )
+    
+    birth_date = models.DateField(
+        blank=True, 
+        null=True,
+        verbose_name="Birth Date"
+    )
+    facebook = models.URLField(
+        max_length=255, 
+        blank=True, 
+        null=True,
+        verbose_name="Facebook Profile"
+    )
+    country = models.CharField(
+        max_length=100, 
+        blank=True, 
+        null=True,
+        verbose_name="Country"
+    )
+    
     STATUS_CHOICES = [
         ('Student', 'Student'),
         ('Graduated', 'Graduated'),
     ]
-    status = models.CharField(max_length=10, choices=STATUS_CHOICES, blank=True, null=True)
-
-    email_active = models.BooleanField(default=False)
-    creat_date = models.DateTimeField(auto_now_add=True)
-
+    status = models.CharField(
+        max_length=10, 
+        choices=STATUS_CHOICES, 
+        blank=True, 
+        null=True,
+        verbose_name="Status"
+    )
+    
+    email_active = models.BooleanField(
+        default=False,
+        verbose_name="Email Activated"
+    )
+    
+    create_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Creation Date"
+    )
+    
     def __str__(self):
         return self.username
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
