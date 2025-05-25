713891cd6af23009a93349eb7562a3b336194f85
Ebramrasmy <ebramrasmy7@gmail.com>
2025-05-25 21:51:44 +0300

    Add serializers for Tag, ProjectTag, and Rate models

---
+++ interactions/API/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from interactions.models import Tag, ProjectTag, Rate
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ['id', 'name']
+        read_only_fields = ['id']
+
+
+class ProjectTagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProjectTag
+        fields = ['id', 'project_id', 'tag_id']
+        read_only_fields = ['id']
+
+
+class RateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rate
+        fields = ['id', 'user_id', 'project_id', 'rate_value', 'note']
+        read_only_fields = ['id', 'user_id']
