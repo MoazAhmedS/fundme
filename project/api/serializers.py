c7a42c2f5e43e9086c1ac744efbc84852bd7288a
gergeskeliny <gergeskeliny60@gmail.com>
2025-05-26 01:26:00 +0300

    creating CategorySerializer, ProjectSerializer and ImageSerializer classes

---
+++ project/api/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers 
+
+from ..models import *
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+        read_only_fields = ['id']
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = '__all__'
+        read_only_fields = ['id','create-date', 'current_donations']
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Images
+        fields = '__all__'
+        read_only_fields = ['id']
