24b3712f804af4cd85657b2edc5db8dfe0a77d10
MoazAhmedS <moazahmed0101@gmail.com>
2025-05-26 14:35:51 +0300

    creating the serializers and views

---
+++ donation/API/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from ..models import Report, ReportComment, ReportProject, Donation
+
+
+class ReportSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Report
+        fields = "__all__"
+
+class ReportCommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ReportComment
+        fields = "__all__"
+
+
+class ReportProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ReportProject
+        fields = "__all__"
+
+
+class DonationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Donation
+        fields = "__all__"
