f1825f6282381f3016a01eb746618acf551bc11e
MoazAhmedS <moazahmed0101@gmail.com>
2025-05-26 20:28:15 +0300

    adding register,login serializer

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -5,3 +5,23 @@ class AccountSerializer(serializers.ModelSerializer):
     class Meta:
         model =  ProfileUser
         fields = '__all__'  
+        
+class AccountRegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model =  ProfileUser
+        fields = [
+            'email',
+            'password',
+            'first_name',
+            'last_name',
+            'phone',
+            'image',
+        ]  
+
+class AccountLoginSerializer(serializers.ModelSerializer):
+    class Meta:
+        model =  ProfileUser
+        fields = [
+            'email',
+            'password',
+        ]
