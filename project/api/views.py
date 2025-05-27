0f2533223e23a70f86558320fe2b8aa36bb80a3c
gergeskeliny <gergeskeliny60@gmail.com>
2025-05-27 16:40:58 +0300

    removing project details endpoint

--- project/api/views.py
+++ project/api/views.py
@@ -30,9 +30,3 @@ class ReadAndCreateProject(APIView):
                 status=status.HTTP_400_BAD_REQUEST
             )
         
-class ReadUpdateDeleteProjectByID(APIView):
-    def get(self,request,id):
-        return Response(
-            data=ProjectSerializer.getProjectById(id),
-            status=status.HTTP_200_OK
-        )
