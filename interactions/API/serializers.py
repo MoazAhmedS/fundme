b9ee58eec117fe823a3e9a16def3b8f2f92b4b52
Ebramrasmy <ebramrasmy7@gmail.com>
2025-05-27 14:46:21 +0300

    Edit serializers.py in app interactions

--- interactions/API/serializers.py
+++ interactions/API/serializers.py
@@ -27,26 +27,4 @@ class RateSerializer(serializers.ModelSerializer):
         if value < 1 or value > 5:
             raise serializers.ValidationError("Rate must be between 1 and 5.")
         return value
-    
-class RateCreateAPIView(generics.CreateAPIView):
-    queryset = Rate.objects.all()
-    serializer_class = RateSerializer
-    permission_classes = [IsAuthenticated]
 
-    def perform_create(self, serializer):
-        serializer.save(user_id=self.request.user.profileuser)
-
-class RateListAPIView(generics.ListAPIView):
-    serializer_class = RateSerializer
-    permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        queryset = Rate.objects.all()
-        project_id = self.request.query_params.get('project_id')
-        user_id = self.request.query_params.get('user_id')
-        
-        if project_id:
-            queryset = queryset.filter(project_id=project_id)
-        if user_id:
-            queryset = queryset.filter(user_id=user_id)
-        return queryset
--- interactions/API/views.py
+++ interactions/API/views.py
@@ -1,5 +1,4 @@
 from rest_framework import generics, permissions
-from rest_framework import generics
 from interactions.models import *
 from interactions.API.serializers import *
 from rest_framework.permissions import IsAuthenticated
@@ -20,3 +19,26 @@ class ProjectTagCreateAPIView(generics.CreateAPIView):
     serializer_class = ProjectTagSerializer
 
 
+class RateCreateAPIView(generics.CreateAPIView):
+    queryset = Rate.objects.all()
+    serializer_class = RateSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user_id=self.request.user.profileuser)
+
+
+class RateListAPIView(generics.ListAPIView):
+    serializer_class = RateSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = Rate.objects.all()
+        project_id = self.request.query_params.get('project_id')
+        user_id = self.request.query_params.get('user_id')
+        
+        if project_id:
+            queryset = queryset.filter(project_id=project_id)
+        if user_id:
+            queryset = queryset.filter(user_id=user_id)
+        return queryset
