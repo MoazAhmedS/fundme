e0c16ec145577a9cb289a0920816cebeffe2f6cd
mahmoudOraby <mahmoud.eelu.2020@gmail.com>
2025-05-27 22:47:51 +0300

    ADDING report project

--- reports/API/views.py
+++ reports/API/views.py
@@ -7,7 +7,7 @@ from ..models import Report, ReportProject, ReportComment
 from project.models import Project
 from comments.models import Comment
 
-class ReportProjectView(APIView):
+class ReportCommentView(APIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request, comment_id):
@@ -21,9 +21,30 @@ class ReportProjectView(APIView):
             report_details=reason
         )
 
-        ReportProject.objects.create(
+        ReportComment.objects.create(
             report_id=report,
             comment_id= comment
         )
 
         return Response({'success': True, 'message': 'comment has been reported successfully.'}, status=status.HTTP_201_CREATED)
+
+class ReportProjectView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, project_id):
+        reason = request.data.get('reason')
+        if not reason:
+            return Response({'success': False, 'message': 'Reason is required.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        project = get_object_or_404(Project, id= project_id)
+        report = Report.objects.create(
+            user_id=request.user.profileuser,
+            report_details=reason
+        )
+
+        ReportProject.objects.create(
+            report_id=report,
+            project_id= project
+        )
+
+        return Response({'success': True, 'message': 'project has been reported successfully.'}, status=status.HTTP_201_CREATED)
--- reports/urls.py
+++ reports/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import ReportCommentAPIView, ReportProjectAPIView
+from .API.views import ReportCommentView, ReportProjectView
 
 urlpatterns = [
-    path('comments/<int:comment_id>/', ReportCommentAPIView.as_view(), name='report-comment'),
-    path('projects/<int:project_id>/', ReportProjectAPIView.as_view(), name='report-project'),
+    path('comments/<int:comment_id>/', ReportCommentView.as_view(), name='report-comment'),
+    path('projects/<int:project_id>/', ReportProjectView.as_view(), name='report-project'),
 ]
