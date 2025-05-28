6f4de7f307403a1d1b4bb51fc318357342dea4e8
abdullah ayman <aymanabdullah016@gmail.com>
2025-05-28 15:59:27 +0300

    adding donation api

--- donation/API/serializers.py
+++ donation/API/serializers.py
@@ -1,22 +1,5 @@
 from rest_framework import serializers
-from ..models import Report, ReportComment, ReportProject, Donation
-
-
-class ReportSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Report
-        fields = "__all__"
-
-class ReportCommentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ReportComment
-        fields = "__all__"
-
-
-class ReportProjectSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ReportProject
-        fields = "__all__"
+from ..models import  Donation
 
 
 class DonationSerializer(serializers.ModelSerializer):
