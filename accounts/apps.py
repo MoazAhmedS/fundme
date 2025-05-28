42bb2a26ae4e7cec4c396ca29fd2982ebc31fcb3
MoazAhmedS <moazahmed0101@gmail.com>
2025-05-28 23:34:30 +0300

    creating signals for email and phone generation from facebook first login

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
+default_app_config = 'accounts.apps.AccountsConfig'
 
 
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
+    def ready(self):
+        import accounts.signals  
---
+++ accounts/signals.py
@@ -0,0 +1,17 @@
+from allauth.socialaccount.signals import pre_social_login
+from django.dispatch import receiver
+import random
+
+def generate_egyptian_phone_number():
+    prefixes = ['010', '011', '012', '015']
+    prefix = random.choice(prefixes)
+    suffix = ''.join(str(random.randint(0, 9)) for _ in range(8))
+    return prefix + suffix
+
+@receiver(pre_social_login)
+def populate_email_if_missing(sender, request, sociallogin, **kwargs):
+    user = sociallogin.user
+    if not user.email:
+        user.email = f"{sociallogin.account.uid}@facebook.com"
+        user.username = user.email  
+        user.phone = generate_egyptian_phone_number()
