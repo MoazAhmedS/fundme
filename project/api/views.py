9a23d474e4ef98b26f8dc69fdecf65c18b53734b
mahmoudOraby <mahmoud.eelu.2020@gmail.com>
2025-05-29 22:59:47 +0300

    adding API  to get projects using category id

--- project/api/views.py
+++ project/api/views.py
@@ -3,7 +3,10 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from project.models import Category
-from project.api.serializers import CategorySerializer
+from project.api.serializers import *
+from django.shortcuts import get_object_or_404
+from project.models import Project
+
 
 class CreateCategoryView(APIView):
     permission_classes = [IsAuthenticated]
@@ -21,3 +24,21 @@ class CreateCategoryView(APIView):
             }, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+ 
+
+class ProjectsByCategoryView(APIView):
+    def get(self, request, category_id):
+        category = get_object_or_404(Category, pk=category_id)
+
+    
+        projects = Project.objects.filter(categoryObject=category)
+
+        if not projects.exists():
+            return Response(
+                {"projects": [], "message": "No projects found for this category."},
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+        serializer = ProjectSerializer(projects, many=True)
+        return Response({"projects": serializer.data}, status=status.HTTP_200_OK)
--- project/urls.py
+++ project/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from project.api.views import CreateCategoryView  
+from project.api.views import CreateCategoryView,ProjectsByCategoryView  
 
 urlpatterns = [
     path('api/createCategory/', CreateCategoryView.as_view(), name='create-category'),
+    path('categories/<int:category_id>/projects/', ProjectsByCategoryView.as_view(), name='projects-by-category'),
 ]
