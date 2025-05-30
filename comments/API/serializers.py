91559525760652d1f8a467fd67fa265e2185e5ba
MoazAhmedS <moazahmed0101@gmail.com>
2025-05-30 21:56:01 +0300

    fixing ProjectCommentsView Serialization

--- comments/API/serializers.py
+++ comments/API/serializers.py
@@ -11,3 +11,14 @@ class CommentReplySerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = ['id', 'user_id', 'project_id', 'parent_id', 'comment', 'created_date']
+
+class CommentSerializer(serializers.ModelSerializer):
+    replies = serializers.SerializerMethodField()
+
+    class Meta:
+        fields = ['id', 'user_id', 'project_id', 'parent_id', 'comment', 'created_date', 'replies']
+
+    def get_replies(self, obj):
+        if obj.replies.exists():
+            return CommentSerializer(obj.replies.all(), many=True).data
+        return []
