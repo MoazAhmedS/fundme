598ef9a89bc6db18747bfc500401e628ef4fb380
gergeskeliny <gergeskeliny60@gmail.com>
2025-05-31 16:43:48 +0300

    Fixing interactions route cause it was interactions/API/API/...

--- interactions/urls.py
+++ interactions/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .API.views import *
 
 urlpatterns = [
-    path('API/projects/<int:project_id>/rate/', RateCreateAPIView.as_view(), name='rate-project'),
-    path('API/projects/rating/<int:project_id>/', RateListAPIView.as_view(), name='get-project-ratings'),
+    path('projects/<int:project_id>/rate/', RateCreateAPIView.as_view(), name='rate-project'),
+    path('projects/rating/<int:project_id>/', RateListAPIView.as_view(), name='get-project-ratings'),
 ]
