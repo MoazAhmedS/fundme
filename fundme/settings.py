2975383e148e7fb3c790cf08eea47b526110dbbd
MoazAhmedS <moazahmed0101@gmail.com>
2025-05-31 15:15:41 +0300

    adding swagger api doc and updating requirements

--- fundme/settings.py
+++ fundme/settings.py
@@ -55,6 +55,8 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.facebook',
     'dj_rest_auth.registration',
     # End Django Allauth Facebook settings
+    'drf_spectacular',
+
 ]
 
 MIDDLEWARE = [
@@ -163,6 +165,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+
 }
 
 
--- fundme/urls.py
+++ fundme/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,4 +28,6 @@ urlpatterns = [
     path('Project/API/',include('project.urls')),
     path('interactions/API/', include('interactions.urls')),
     path('donation/API/', include('donation.urls')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
