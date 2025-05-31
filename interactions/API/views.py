764e201f963b26d10d4a94579ec54da89c7ae839
MoazAhmedS <moazahmed0101@gmail.com>
2025-05-31 17:20:12 +0300

    adding API to create tags if not exist

--- interactions/API/views.py
+++ interactions/API/views.py
@@ -3,7 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from interactions.models import Rate
-from .serializers import RateSerializer
+from .serializers import *
 
 class RateListAPIView(APIView):
     def get(self, request, project_id):
@@ -22,3 +22,20 @@ class RateCreateAPIView(APIView):
             serializer.save(user=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class CreateTagView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        serializer = TagSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        name = serializer.validated_data['name'].strip()
+
+        if not name:
+            return Response({"name": "Tag name cannot be empty."}, status=status.HTTP_400_BAD_REQUEST)
+
+        tag, created = Tag.objects.get_or_create(name=name)
+        output = TagSerializer(tag).data
+        return Response(output | {"created": created}, status=status.HTTP_201_CREATED if created else status.HTTP_200_OK)
--- interactions/urls.py
+++ interactions/urls.py
@@ -4,4 +4,5 @@ from .API.views import *
 urlpatterns = [
     path('projects/<int:project_id>/rate/', RateCreateAPIView.as_view(), name='rate-project'),
     path('projects/rating/<int:project_id>/', RateListAPIView.as_view(), name='get-project-ratings'),
+    path('tags/create/', CreateTagView.as_view()),
 ]
